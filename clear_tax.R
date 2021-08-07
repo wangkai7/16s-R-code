7c5d4d89c361754d76715f63884196b07865f8fc
wangkai7 <57486545+wangkai7@users.noreply.github.com>
2021-08-07 22:40:54 +0800

    Create clear_tax.R
    Make a tidy format of taxonomy file.

---
+++ clear_tax.R
@@ -0,0 +1,20 @@
+## tax: ASV's taxonomic annotation based on silva data base. Loaded in R with rownames.
+function(tax){
+  tax<-data.frame(str_split_fixed(tax$Taxon,";",7),row.names = rownames(tax))
+  tax<-apply(tax, 2, function(x){
+    x[grep("^[^D]",x)]<-""
+    return(x)
+  })
+  tax<-apply(tax,2,function(x)substring(x,6))
+  tax<-apply(tax,2,function(x){
+    x[x==""]<-"Unassigned"
+    return(x)
+  })
+  for (i in 1:7) {
+    pf<-c("k","p","c","o","f","g","s")
+    tax[,i]<-paste0(pf[i],"_",tax[,i])
+  }
+  tax<-as.data.frame(tax)
+  colnames(tax)<-c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+  return(tax)
+}
